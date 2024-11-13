8dc5486ede2c39a5b080c7cebad7c384a37330c5
drDeepl <vlimaster@yandex.ru>
2024-11-13 11:07:31 +0700

    feat(welcome.page): added img bg

--- src/pages/welcome.page.tsx
+++ src/pages/welcome.page.tsx
@@ -15,28 +15,30 @@ const WelcomePage: React.FC = () => {
   };
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.9, ease: "easeOut" }}
-    >
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        <div className="flex flex-col items-center">
-          <h1 className="text-slate-700 text-3xl font-bold mb-3">{title}</h1>
-          <p className="text-slate-600 text-center text-balance text-balance">
-            {description}
-          </p>
+    <div className="bg-[url('./welcome-page-bg.png')] bg-center bg-scroll">
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.9, ease: "easeOut" }}
+      >
+        <div className="min-h-screen flex flex-col items-center justify-center backdrop-blur-sm bg-gradient-to-t from-orange-50">
+          <div className="flex flex-col items-center">
+            <h1 className="text-slate-700 text-3xl font-bold mb-3">{title}</h1>
+            <p className="text-slate-600 text-center text-balance text-balance">
+              {description}
+            </p>
+          </div>
+          <div className="flex flex-around space-x-2">
+            <Button
+              className="mt-4 bg-orange-600  hover:bg-slate-100 hover:text-slate-700 animate-jump-heart"
+              onClick={handleOnMap}
+            >
+              карта
+            </Button>
+          </div>
         </div>
-        <div className="flex flex-around space-x-2">
-          <Button
-            className="mt-4 bg-orange-600  hover:bg-slate-100 hover:text-slate-700 animate-jump-heart"
-            onClick={handleOnMap}
-          >
-            карта
-          </Button>
-        </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
 
