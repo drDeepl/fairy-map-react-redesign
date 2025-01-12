88819e22f5e28af6b744ff9bad9254c1b6186b5c
drDeepl <vlimaster@yandex.ru>
2025-01-12 22:40:38 +0700

    feat(features\user\user.page): added btns action in header

--- src/features/user/user.page.tsx
+++ src/features/user/user.page.tsx
@@ -20,18 +20,20 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { RouteApp } from "@/pages/constants/route.enum";
-
-interface StoryWithImgResponseDto
-  extends Components.Schemas.StoryWithImgResponseDto {}
+import { ExitFullScreenIcon, ExitIcon } from "@radix-ui/react-icons";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { MapPinnedIcon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface UserBookAudioState {
   load: boolean;
-  books: StoryWithImgResponseDto[];
+  books: Components.Schemas.StoryWithImgResponseDto[];
 }
-
-// interface SelectedBookViewState {
-//   book: StoryWithImgResponseDto | null;
-// }
 
 const UserPage: React.FC = () => {
   const { user }: AuthState = useSelector((state: RootState) => state.auth);
@@ -46,11 +48,6 @@ const UserPage: React.FC = () => {
       books: [],
     });
 
-  // const [selectedBookViewState, setSelectedBookViewState] =
-  //   useState<SelectedBookViewState>({
-  //     book: null,
-  //   });
-
   useEffect(() => {
     if (!user) {
       navigate(-1);
@@ -62,13 +59,17 @@ const UserPage: React.FC = () => {
         .get({
           userId: Number(user.sub),
         })
-        .then((res: AxiosResponse<StoryWithImgResponseDto[]>) => {
-          setUserAudioStoriesState({
-            load: false,
-            books: res.data,
-          });
-          console.log(userAudioStoriesState);
-        });
+        .then(
+          (
+            res: AxiosResponse<Components.Schemas.StoryWithImgResponseDto[]>
+          ) => {
+            setUserAudioStoriesState({
+              load: false,
+              books: res.data,
+            });
+            console.log(userAudioStoriesState);
+          }
+        );
     }
   }, []);
 
@@ -95,30 +96,54 @@ const UserPage: React.FC = () => {
 
   return (
     <div className="flex flex-col justify-center">
-      <div className="w-full flex justify-end px-4 py-4">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              className="rounded-full border border-slate-800 bg-slate-100 size-11 shadow-md"
-              variant="ghost"
-              size="icon"
-              onClick={() => handleOnClickAvatar()}
-            >
-              <span className="text-black">
-                {user?.firstName[0].toUpperCase()}
-              </span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <Button
-              variant="ghost"
-              className="w-full"
-              onClick={handleOnClickLogout}
-            >
-              выйти
-            </Button>
-          </DropdownMenuContent>
-        </DropdownMenu>
+      <div className="sticky w-full flex justify-between items-center px-4 py-4 pl-20 shadow-md rounded-md">
+        {/* <Button
+          variant="ghost"
+          size="icon"
+          className="size-11 rounded-full border border-slate-800 bg-slate-100"
+          onClick={handleOnClickLogout}
+        >
+          <ExitIcon />
+        </Button> */}
+        <p className="text-black font-semibold text-xl capitalize">
+          Привет, {user?.firstName}
+        </p>
+
+        <div className="space-x-2">
+          <TooltipProvider delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  className="rounded-full text-slate-600 border border-baby-blue-800 bg-baby-blue-50 size-11 shadow-md"
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => navigate(RouteApp.MapPage)}
+                >
+                  <MapPinnedIcon />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>к карте</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  className="rounded-full text-slate-600 border border-baby-blue-800 bg-baby-blue-50 size-11 shadow-md"
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => handleOnClickLogout()}
+                >
+                  {/* <span className="text-black text-xl">
+            {user?.firstName[0].toUpperCase()}
+          </span> */}
+                  <ExitIcon />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>выйти</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
       <div className="px-24 py-4">
         <div>Книги с моей озвучкой</div>
