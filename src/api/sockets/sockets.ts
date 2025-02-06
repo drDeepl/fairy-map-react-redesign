aeb93397e03b0c7af400741668d734d9b5c014d8
drDeepl <vlimaster@yandex.ru>
2025-02-06 16:25:13 +0700

    refactor: .env

--- src/api/sockets/sockets.ts
+++ src/api/sockets/sockets.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const url = "http://vleep-deep.ru:3000";
+const url = `${import.meta.env.VITE_WEBSOCKET_IO_API_URL}`;
 
 export const socket = io(url, {
   transports: ["websocket"],
