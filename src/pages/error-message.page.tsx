5754c7fb9a7002b28284f21566f1ee5fc14f0cd4
drDeepl <vlimaster@yandex.ru>
2025-02-18 12:14:37 +0700

    refactor(src\pages\error-message.page.tsx): changed redirect to main page

--- src/pages/error-message.page.tsx
+++ src/pages/error-message.page.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
-import { ArrowLeftIcon, CrossCircledIcon } from "@radix-ui/react-icons";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
 interface ErrorMessageProps {
   message: string;
@@ -10,22 +10,21 @@ const ErrorMessageScreen: React.FC<ErrorMessageProps> = ({ message }) => {
   const navigate = useNavigate();
 
   const handleOnBack = () => {
-    navigate(-1);
+    navigate("/");
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="absolute bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 filter blur-3xl h-2/6 w-1/6 left-2 top-1"></div>
-      <div className="absolute bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 filter blur-3xl h-2/6 w-1/6 right-2 bottom-1"></div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="absolute w-1/6 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 filter blur-3xl h-2/6 left-2 top-1"></div>
+      <div className="absolute w-1/6 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 filter blur-3xl h-2/6 right-2 bottom-1"></div>
       <div className="relative z-30 flex flex-col items-center text-center ">
         <div className="flex flex-col justify-center">
-          <CrossCircledIcon className="self-center mb-3 size-14 text-red-400" />
-          <h1 className="text-slate-500 text-4xl font-bold self-start">Упс!</h1>
+          <h1 className="self-start text-4xl font-bold text-slate-500">Упс!</h1>
         </div>
-        <h1 className="mt-2 text-slate-500 text-2xl">{message}...</h1>
+        <h1 className="mt-2 text-2xl text-slate-500">{message}...</h1>
         <Button className="m-2" onClick={handleOnBack} variant="outline">
           <ArrowLeftIcon />
-          назад
+          на главную
         </Button>
       </div>
     </div>
